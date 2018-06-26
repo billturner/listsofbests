c0c2ef0089eba974c7cad88b2bdeb5a041c3bf0f
Bill Turner <billturner@gmail.com>
2018-06-26 01:28:40 -0400

    a few config changes

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module ListsOfBests
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # default to using UUIDs
+    config.generators do |generator|
+      generator.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
